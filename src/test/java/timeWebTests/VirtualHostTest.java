119aa4a8575a499781e9245f4ba3ba4a3317b74e
kirill <rroogg76aacchh@gmail.com>
2022-06-14 16:10:57 +0300

    Tests for main page added

---
+++ src/test/java/timeWebTests/VirtualHostTest.java
@@ -0,0 +1,42 @@
+package timeWebTests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Dimension;
+import timeWeb.LoginPage;
+import timeWeb.MainPage;
+import util.BrowserDrivers;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class VirtualHostTest {
+    static BrowserDrivers browserDrivers;
+    static HashMap<String, MainPage> mainPages;
+
+    @BeforeAll
+    public static void loadPage() throws IOException {
+        browserDrivers = new BrowserDrivers();
+        mainPages = new HashMap();
+        browserDrivers.drivers.forEach((key, driver) -> driver.get("https://wm.timeweb.ru/login?_ga=2.196343784.555368092.1655207883-2128871962.1655207883"));
+        browserDrivers.drivers.forEach((key, driver) -> driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS));
+        browserDrivers.drivers.forEach((key, driver) -> mainPages.put(key, new MainPage(driver)));
+        browserDrivers.drivers.forEach((key, driver) -> driver.manage().window().setSize(new Dimension(1024, 1024)));
+    }
+
+    @BeforeEach
+    @AfterEach
+    public void timeOut() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    @Test
+    @DisplayName("ClickVirtualHosting")
+    public void clickVirtualHosting() throws InterruptedException {
+        mainPages.forEach((key, mainPage) -> assertTrue(mainPage.clickVirtualHosting()));
+    }
+
+
+}
